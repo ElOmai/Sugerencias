3c5564374347aac0b8bd1280c65fe4484174097f
Luis Omar Emilio Perez Rodriguez <luis_perez6@ucne.edu.do>
2019-10-02 19:27:12 -0400

    Add project files.

---
+++ Sugerencias/Registros/rSugerencias.aspx.cs
@@ -0,0 +1,129 @@
+﻿using BLL;
+using Entidades;
+using Sugerencias.Registros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sugerencias.Registros
+{
+    public partial class rSugerencias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                FechaTextBox.Text = DateTime.Now.ToFormatDate();
+            }
+        }
+        private void Limpiar()
+        {
+            SugerenciasIdTextBox.Text = 0.ToString();
+            FechaTextBox.Text = DateTime.Now.ToString();
+            DescripcionTextBox.Text = string.Empty;
+        }
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        private Sugerencia LlenaClase()
+        {
+            return new Sugerencia()
+            {
+                SugerenciasId = SugerenciasIdTextBox.Text.ToInt(),
+                Fecha = FechaTextBox.Text.ToDatetime(),
+                Descripcion = DescripcionTextBox.Text
+            };
+        }
+        private void LlenaCampo(Sugerencia sugerencia)
+        {
+            SugerenciasIdTextBox.Text = sugerencia.SugerenciasId.ToString();
+            FechaTextBox.Text = sugerencia.Fecha.ToString();
+            DescripcionTextBox.Text = sugerencia.Descripcion;
+        }
+        private bool Validar()
+        {
+            bool paso = true;
+            if (SugerenciasIdTextBox.Text.EsNulo() || string.IsNullOrWhiteSpace(SugerenciasIdTextBox.Text))
+                paso = false;
+            if (string.IsNullOrWhiteSpace(FechaTextBox.Text))
+                paso = false;
+            if (string.IsNullOrWhiteSpace(DescripcionTextBox.Text))
+                paso = false;
+            return paso;
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            RepositorioBase<Sugerencia> repositorio = new RepositorioBase<Sugerencia>();
+            Sugerencia sugerencias = repositorio.Buscar(DescripcionTextBox.Text.ToInt());
+            return sugerencias.EsNulo();
+        }
+        protected void GuadarButton_Click(object sender, EventArgs e)
+        {
+            if (!Validar())
+                return;
+
+            RepositorioBase<Sugerencia> repositorio = new RepositorioBase<Sugerencia>();
+            Sugerencia sugerencias = LlenaClase();
+            TipoTitulo tipoTitulo = TipoTitulo.OperacionFallida;
+            TiposMensajes tiposMensajes = TiposMensajes.RegistroNoGuardado;
+            IconType iconType = IconType.error;
+            bool paso = false;
+
+            if (sugerencias.SugerenciasId == 0)
+                paso = repositorio.Guardar(sugerencias);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    Utils.ToastSweet(this, IconType.info, TiposMensajes.RegistroNoEncontrado);
+                    return;
+                }
+                paso = repositorio.Modificar(sugerencias);
+            }
+            if (paso)
+            {
+                Limpiar();
+                tipoTitulo = TipoTitulo.OperacionExitosa;
+                tiposMensajes = TiposMensajes.RegistroGuardado;
+                iconType = IconType.success;
+            }
+            Utils.Alerta(this, tipoTitulo, tiposMensajes, iconType);
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Sugerencia> repositorio = new RepositorioBase<Sugerencia>();
+            int id = SugerenciasIdTextBox.Text.ToInt();
+            if (!ExisteEnLaBaseDeDatos())
+            {
+                Utils.Alerta(this, TipoTitulo.OperacionFallida, TiposMensajes.RegistroInexistente, IconType.error);
+                return;
+            }
+            else
+            {
+                if (repositorio.Eliminar(id))
+                {
+                    Utils.Alerta(this, TipoTitulo.OperacionExitosa, TiposMensajes.RegistroEliminado, IconType.success);
+                    Limpiar();
+                }
+            }
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Sugerencia> repositorio = new RepositorioBase<Sugerencia>();
+            Sugerencia Sugerencias = repositorio.Buscar(SugerenciasIdTextBox.Text.ToInt());
+            if (!Sugerencias.EsNulo())
+            {
+                Limpiar();
+                LlenaCampo(Sugerencias);
+            }
+            else
+                Utils.ToastSweet(this, IconType.info, TiposMensajes.RegistroNoEncontrado);
+        }
+    }
+}
---
+++ Sugerencias/Utilitarios/Utils.cs
@@ -0,0 +1,28 @@
+﻿using Entidades;
+using System.Web.UI;
+
+namespace Sugerencias
+{
+    public class Utils
+    {
+        public static void Alerta(System.Web.UI.Page page, TipoTitulo Titulo, TiposMensajes Mensaje, IconType iconType)
+        {
+            string TituloDescripcion = Titulo.GetDescription();
+            string MensajeDescripcion = Mensaje.GetDescription();
+            string iconTypeDescripcion = iconType.ToString();
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "alert",
+                            $"sweetalert('{TituloDescripcion}','{MensajeDescripcion}','{iconTypeDescripcion}')", true);
+        }
+        public static void ToastSweet(System.Web.UI.Page page, IconType iconType, TiposMensajes Mensaje)
+        {
+            string IconTypeDescripcion = iconType.ToString();
+            string MensajeDescripcion = Mensaje.GetDescription();
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "alert",
+                            $"ToastSweetAlert('{IconTypeDescripcion}','{MensajeDescripcion}')", true);
+        }
+        public static void MostrarModal(System.Web.UI.Page page, string NombreModal, string Titulo)
+        {
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "Popup", $"{NombreModal}('{ Titulo }');", true);
+        }
+    }
+}
